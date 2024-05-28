6a78874decce3cba71888a24ae243ca61fc888f3
Pablo Melo <panmecar@gmail.com>
2024-05-27 19:31:42 -0500

    fix(deploy):deploy 27 may

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Billify Landing Page',
+      title: 'Movies Gallery Landing Page',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
@@ -20,11 +22,62 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
 
   @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  bool isHide = false;
+  @override
   Widget build(BuildContext context) {
+    const String privacyPolicy = '''
+Última actualización: 27/05/2024
+
+Introducción
+
+Esta Política de Privacidad describe cómo Movies Gallery la Aplicación recopila, utiliza y comparte información sobre usted cuando utiliza nuestra aplicación móvil. Al usar nuestra aplicación, usted acepta las prácticas descritas en esta Política de Privacidad.
+
+Información que Recopilamos
+
+Información que No Recopilamos
+
+Nuestra aplicación no recopila ni almacena información personal de los usuarios. No solicitamos, accedemos ni almacenamos datos como nombres, direcciones de correo electrónico, números de teléfono, ubicaciones o cualquier otro tipo de información personal.
+
+Información Automática
+
+La única información que nuestra aplicación procesa son los datos obtenidos de una API pública. Estos datos son directamente consultados y mostrados en la aplicación sin ninguna modificación o almacenamiento.
+
+Uso de la Información
+
+Utilizamos los datos obtenidos de la API pública exclusivamente para mostrar información relevante en la aplicación. Esta información se utiliza para proporcionar a los usuarios datos actualizados y relevantes según el propósito de la aplicación.
+
+Compartir Información
+
+No compartimos ninguna información personal con terceros ya que no recopilamos ninguna. Los datos de la API pública mostrados en la aplicación son obtenidos directamente de la fuente pública y no se transmiten a otros servicios o terceros.
+
+Seguridad
+
+Nos comprometemos a proteger la información que procesamos en la aplicación. Implementamos medidas de seguridad razonables para proteger los datos obtenidos de la API pública contra acceso no autorizado, alteración, divulgación o destrucción.
+
+Cambios en esta Política de Privacidad
+
+Podemos actualizar nuestra Política de Privacidad de vez en cuando. Le notificaremos cualquier cambio publicando la nueva Política de Privacidad en esta página. Se recomienda revisar esta Política de Privacidad periódicamente para cualquier cambio.
+
+Contacto
+
+Si tiene alguna pregunta sobre esta Política de Privacidad, por favor contáctenos a través de:
+
+Correo electrónico: pablo.melo0420@gmail.com
+
+
+Aceptación de esta Política
+
+Al utilizar nuestra aplicación, usted acepta esta Política de Privacidad. Si no está de acuerdo con esta política, por favor no utilice nuestra aplicación.
+''';
+
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
@@ -62,6 +115,44 @@ class LandingPage extends StatelessWidget {
               ),
 
               SizedBox(height: size.height * 0.1),
+
+              Container(
+                color: Colors.transparent,
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: const Text(
+                  " Política de Privacidad",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              if (!isHide)
+                Container(
+                  color: Colors.transparent,
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: const Text(privacyPolicy),
+                ),
+
+              SizedBox(
+                width: size.width * 0.2,
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isHide = !isHide;
+                    });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(!isHide
+                          ? Icons.keyboard_arrow_up_outlined
+                          : Icons.keyboard_arrow_down_outlined),
+                      SizedBox(
+                        width: size.width * 0.05,
+                      ),
+                      Text(!isHide ? "Ocultar" : "Mostrar"),
+                    ],
+                  ),
+                ),
+              ),
 
               // Heading
               const Padding(
@@ -111,32 +202,41 @@ class LandingPage extends StatelessWidget {
                     child: const Text('GET IT ON\nGoogle Play'),
                   ),
                   const SizedBox(width: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      // TODO: Implement App Store link
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 30, vertical: 15),
-                      textStyle: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    child: const Text('Download on the\nApp Store'),
-                  ),
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //   },
+                  //   style: ElevatedButton.styleFrom(
+                  //     backgroundColor: Colors.black,
+                  //     padding: const EdgeInsets.symmetric(
+                  //         horizontal: 30, vertical: 15),
+                  //     textStyle: const TextStyle(
+                  //       color: Colors.white,
+                  //       fontSize: 16,
+                  //       fontWeight: FontWeight.bold,
+                  //     ),
+                  //   ),
+                  //   child: const Text('Download on the\nApp Store'),
+                  // ),
                 ],
               ),
 
               // Image/Device Preview (Optional)
-              Container(
-                margin: EdgeInsets.only(
-                    bottom: size.height * 0.1, top: size.height * 0.1),
-                height: size.height * 0.8,
-                child: Image.asset(
-                    'assets/images/screenshot_app.png'), // Replace with your image
+              GestureDetector(
+                onTap: () {
+                  debugPrint("clickeo");
+                },
+                child: MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      bottom: size.height * 0.1,
+                    ),
+                    height: size.height * 0.5,
+                    // color: Colors.red,
+                    child: Image.asset(
+                        'assets/images/Movies Gallery.png'), // Replace with your image
+                  ),
+                ),
               ),
             ],
           ),
